5ff825a15023dbe4c326ee336ab40a0d1d09f16d
Gabriel Guedes Flores <gabriel.flores@e-unicred.com.br>
2023-06-11 17:19:37 -0300

    feat: adicionado fluxos de produtos

--- src/controller/products/products.module.ts
+++ src/controller/products/products.module.ts
@@ -3,9 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsController } from './products.controller';
 import { ProductsService } from '../../services/products/products.service';
 import { Product, ProductSchema } from '../../schemas/products/products.schema';
+import { OrderItem, OrderItemSchema } from 'src/schemas/order-item/order-item.schema';
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }])],
+    imports: [
+        MongooseModule.forFeature([
+            { name: Product.name, schema: ProductSchema },
+            { name: OrderItem.name, schema: OrderItemSchema }
+        ])],
     controllers: [ProductsController],
     providers: [ProductsService],
 })
--- src/services/products/products.service.ts
+++ src/services/products/products.service.ts
@@ -2,16 +2,65 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { IProductCreateDTO } from 'src/controller/products/dto/product-create.dto';
+import { OrderItem, OrderItemDocument } from 'src/schemas/order-item/order-item.schema';
 import { Product, ProductDocument } from 'src/schemas/products/products.schema';
 
 @Injectable()
 export class ProductsService {
-    constructor(@InjectModel(Product.name) private productModel: Model<ProductDocument>) { }
+    constructor(
+        @InjectModel(Product.name) private productModel: Model<ProductDocument>,
+        @InjectModel(OrderItem.name) private orderItemModel: Model<OrderItemDocument> 
+    ) { }
+    
 
     async create(bodyProduct: ProductDocument): Promise<IProductCreateDTO> {
-        console.log('body service create', bodyProduct);
         const createdProduct = new this.productModel(bodyProduct);
         await createdProduct.save();
         return { status: "created" };
     }
+
+    async findAll(): Promise<ProductDocument[]> {
+        return this.productModel.find().exec();
+    }
+
+    async update(productId: string, updatedProduct: ProductDocument): Promise<ProductDocument> {
+        return this.productModel.findByIdAndUpdate(productId, updatedProduct, { new: true });
+    }
+
+    async delete(productId: string): Promise<any> {
+        return this.productModel.findByIdAndRemove(productId);
+    }
+
+    async findProductsWithOrders(): Promise<any> {
+        return this.productModel.aggregate([
+            {
+                $lookup: {
+                    from: "orderitems",
+                    localField: '_id',
+                    foreignField: 'productId',
+                    as: 'orderitems'
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    name: 1,
+                    description: 1,
+                    price: 1,
+                    category: 1,
+                    orderItems: {
+                        $map: {
+                            input: "$orderitems",
+                            as: "item",
+                            in: {
+                                quantity: "$$item.quantity",
+                                orderId: "$$item.orderId"
+                            }
+                        }
+                    }
+                }
+            }
+        ]).exec();
+    }
+    
 }
